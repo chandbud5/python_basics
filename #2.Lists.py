33a407e0886c590873793264cde4ad2d368e1bba
Chand Bud <47664980+chandbud5@users.noreply.github.com>
2020-01-22 19:37:06 +0530

    Update #2.Lists.py

--- #2.Lists.py
+++ #2.Lists.py
@@ -1,3 +1,9 @@
+
+#1 Create a list of minimum 5 elements
+#2 List out element at 3rd index
+#3 Add extra element to current list
+#4 Add another element at 3rd index in current list
+
 x = [2,3,7,5,0] # List created
 print(x[3]) # 3rd element printed
 x.append(8) #element inserted at the end of list
