0765c4ca24df6f195b6e4b80e6512605cc16b592
Chand Bud <47664980+chandbud5@users.noreply.github.com>
2020-01-22 19:31:42 +0530

    Add files via upload

---
+++ #1.Helloworld.py
@@ -0,0 +1,1 @@
+print("Hello World this repository contains basic python programs")
---
+++ #2.Lists.py
@@ -0,0 +1,6 @@
+x = [2,3,7,5,0] # List created
+print(x[3]) # 3rd element printed
+x.append(8) #element inserted at the end of list
+print(x)
+x.insert(3,6) #element inserted at the 3rd position
+print(x)
