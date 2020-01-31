888bf7eae828b2f8824119653d8ac037a9551754
Chand Bud <47664980+chandbud5@users.noreply.github.com>
2020-01-31 17:50:16 +0530

    Delete #7.Dictionaries.py

--- #7.Dictionaries.py
+++
@@ -1,8 +0,0 @@
-#create Dictionary with atleast five entries init
-#Write code which accepts name of a product and in turn returns their respective prices.
-
-laptop = {"Lenovo" : 40000, "Hp" : 55000, "Dell" : 50000, "Acer":35000 , "Apple":70000}
-
-x = input("Enter company's name to get laptop price")
-print("price of "+ x +"'s laptop is Rs. ")
-print(laptop[x])
